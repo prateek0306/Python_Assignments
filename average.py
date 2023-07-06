09a1e1becfb55cdc61fed6377fc4f394d5b022fe
prateek0306 <136629933+prateek0306@users.noreply.github.com>
2023-07-06 23:28:02 +0530

    Add files via upload

--- average.py
+++ average.py
@@ -2,4 +2,4 @@ n1=float(input('Enter n1: '))
 n2=float(input('Enter n2: '))
 n3=float(input('Enter n3: '))
 average=(n1+n2+n3)/3
-print('Average is:', average)
+print('Average of ',n1,',',n2,'and',n3,'is: ',average )
