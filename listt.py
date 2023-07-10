c764487e631abdbfd0a0d0d0da81872ac81dbc4f
prateek0306 <136629933+prateek0306@users.noreply.github.com>
2023-07-10 22:05:40 +0530

    Add files via upload

---
+++ listt.py
@@ -0,0 +1,33 @@
+# take inputs from user
+numbers=int(input('enter number of elements of list: '))
+list = []
+for i in range(numbers):
+    n=int(input('Enter list numbers: '))
+    list.append(n)
+print('Initial List',list)
+
+# sum of all elements
+print('Sum of elements in list: ',sum(list))
+
+# largest element in list
+print('Largest element in list: ',max(list))
+
+# smallest element in list
+print('Smallest element in list: ',min(list))
+
+# sorting in ascending order
+list.sort()
+print('Ascending order: ',list)
+
+
+# sorting in descending order
+list.reverse()
+print('Descending order: ',list)
+
+# converting into tuple
+t1=tuple(list)
+print('Converted list: ',t1)
+
+# delete list
+del t1
+print('List is deleted successfully')
