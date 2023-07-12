b442e99bdc55472f3d80efc964cdf28cf61650c2
prateek0306 <136629933+prateek0306@users.noreply.github.com>
2023-07-12 20:23:10 +0530

    Add files via upload

--- file_function.py
+++ file_function.py
@@ -1,24 +1,13 @@
-def write_student_info(roll_number, name, class_name, file="student_info.txt"):
-    try:
-        file_obj = open(file, "a")
-        student_data = f"{roll_number}, {name}, {class_name}\n"
-        file_obj.writelines(student_data)
-        file_obj.close()
-    except IOError as e:
-        print(f"An error occurred while opening or writing to the file: {e}")
+def file_func(file='assign7.txt'):
+    try:  
+      f=open(file,'+a')
+      f.writelines([' roll no-43\n','name-prateek\n','class-TYCO'])
+      f.seek(0)
+      print(f.readlines())
+      f.close()
+    except IOError:
+       print('IOError raised')
+file_func()
 
-def print_student_info(file="student_info.txt"):
-    try:
-        file_obj = open(file, "r")
-        data = file_obj.readlines()
-        file_obj.close()
-        print("Student Information:")
-        for line in data:
-            print(line.strip())
-    except IOError as e:
-        print(f"An error occurred while opening or reading the file: {e}")
 
-# Example usage:
-write_student_info(1, "John Doe", "Class A")
-write_student_info(2, "Jane Smith", "Class B")
-print_student_info()
+
